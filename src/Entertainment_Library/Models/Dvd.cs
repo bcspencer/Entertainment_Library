f72ee110f063639ca0cddac9aad1860fe54a4f05
bcspencer <bcspencer25@gmail.com>
2017-01-04 10:16:14 +0000

    Initial commit of the code

---
+++ src/Entertainment_Library/Models/Dvd.cs
@@ -0,0 +1,10 @@
+﻿namespace Entertainment_Library.Models
+{
+    public class Dvd
+    {
+        public int Id { get; set; }
+        public bool HaveACopy { get; set; }
+        public float Price { get; set; }
+
+    }
+}
